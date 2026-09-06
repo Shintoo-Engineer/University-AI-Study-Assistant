bd4f6d9c09ae93a75e4791c060e19b1e861b4b8b
S.S. Shintoo Shel Lal <shintoo.6385@gmail.com>
2026-09-06 15:54:09 +0530

    Connect frontend to public RAG backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     setSources([]);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/ask", {
+      const response = await fetch("https://christina-silence-standings-deal.trycloudflare.com/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
